e7138736f72cdc58466cd071f6e9fd2c55177ee2
SteveRoars <steveroars@gmail.com>
2026-08-13 13:24:23 -0600

    Update TWITCH_CLIENT_ID to web app client ID uruqm8g5qqisy395tk312ph08e138f

--- config.js
+++ config.js
@@ -5,8 +5,7 @@ const CONFIG = {
     API_BASE_URL: "https://streamfish-1ad.e.jrnm.app",
     
     // Twitch Developer Console Client ID for Login with Twitch OAuth
-    // Replace with standard Twitch Application Client ID from dev.twitch.tv/console/apps
-    TWITCH_CLIENT_ID: "959yd4kz9y1kn7c12bqadra92pfyrp",
+    TWITCH_CLIENT_ID: "uruqm8g5qqisy395tk312ph08e138f",
     
     // Dynamic OAuth Redirect URI matching current site location
     REDIRECT_URI: window.location.origin + window.location.pathname,
